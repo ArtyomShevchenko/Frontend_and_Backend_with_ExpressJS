c3fc96b34cd4b4e48d119cb92127b94c1e5745a7
ArtyomShevchenko <artyomshevchenkowarmgray@gmail.com>
2022-10-20 17:38:34 +0300

    add test.html

--- server/index.js
+++ server/index.js
@@ -20,10 +20,30 @@ fs.readdir(__dirname + "/client", (err, files) => {
 })
 
 setTimeout(() => {
-
     app.listen(PORT, () => {
         console.log(`Server is listener on PORT:${colors.bgBlue(`http://localhost:${colors.bgRed(PORT)}`)}`)
     })
+
+    // test start
+
+    app.route('/test')
+        .get(function (req, res) {
+            res.sendFile(path.resolve(__dirname, "client", "test.html"));
+            console.log("get")
+        })
+        .post(function (req, res) {
+            const date = new Date()
+
+            res.send(`Data send: ${date}`);
+            console.log(colors.bgMagenta("post"), date)
+
+            console.log(req.body)
+        })
+        .put(function (req, res) {
+            res.send('Update the book');
+            console.log("put")
+        });
+    // test end
 
     routes.forEach(route => {
         app.get(`/${route}`, (req, res) => {
@@ -31,9 +51,11 @@ setTimeout(() => {
         })
     })
 
+
     app.get(`/`, (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", `index.html`))
     })
+
 
     app.get(`/*`, (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", `error.html`))
